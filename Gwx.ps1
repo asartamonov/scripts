be921178b4f4cc3dae3245c93f862a8358312499
asartamonov <asartamonov@gmail.com>
2016-04-24 13:02:38 +0300

    rm

--- Gwx.ps1
+++
@@ -1,5 +0,0 @@
-﻿#request system for process named GWX and silently continue if not present
-$gwxRunning = Get-Process GWX -ErrorAction SilentlyContinue
-
-#shut it down if GWX launched
-if ($gwxRunning.Id -ne $null) {Stop-Process $gwxRunning.Id}

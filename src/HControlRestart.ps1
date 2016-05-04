516a34898ec7abaf291fc0c45368d3914f7b10d5
asartamonov <asartamonov@gmail.com>
2016-05-04 13:50:52 +0300

    HControl for asus laptop restart process demo

---
+++ src/HControlRestart.ps1
@@ -0,0 +1,13 @@
+﻿#look at process named HControl
+$HCprocess = Get-Process HControl -ErrorAction SilentlyContinue
+
+#check if the process is running shut it down
+if ($HCprocess.Id -ne $null) {
+    Stop-Process $HCprocess.Id
+}
+
+#wait 5 seconds then restart process
+Start-Sleep -s 5
+
+#start process back
+Start-Process -FilePath "C:\Program Files (x86)\ASUS\ATK Package\ATK Hotkey\HControl.exe"

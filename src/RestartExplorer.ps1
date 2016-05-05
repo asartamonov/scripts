8b3a2f74fc71aefae17db9d4136cda57e44fc420
asartamonov <asartamonov@gmail.com>
2016-05-05 20:20:39 +0300

    restart process in Windows sample

---
+++ src/RestartExplorer.ps1
@@ -0,0 +1,11 @@
+﻿#restart explorer.exe if it works incorrectly
+
+#obtain process object and assign it to variable $exp
+$exp = Get-Process explorer
+
+#stop the process 
+Stop-Process $exp
+
+#wait 1 sec than start explorer.exe back
+Start-Sleep -s 1
+Start-Process 'C:\Windows\explorer.exe'
